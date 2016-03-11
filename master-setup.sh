7d309fc49405dfe458bcd46efa84d13cd875a113
Victor Sky <skylandr@users.noreply.github.com>
2016-03-11 15:45:59 +0200

    many small edits

--- master-setup.sh
+++ master-setup.sh
@@ -20,7 +20,7 @@ sonarr() {
         SONARR_INIT_F=/etc/init.d/nzbdrone
         SONARR_REPO_F=/etc/apt/sources.list.d/sonarr.list
         MONO_VER=$(mono -V | grep version | awk '{print $5}')
-        echo Installing Sonarr on your system ... please wait
+        echo Installing Sonarr on $HOSTNAME ... please wait
 ### this section will add the repo and install the key for the sonarr repo
         if [ ! -f $SONARR_REPO_F ]
           then
@@ -32,22 +32,24 @@ sonarr() {
             echo The .list file already exists: $SONARR_REPO_F 2>&1 >> setup.log
         fi
         echo Updating the system: $HOSTNAME ...
-        #apt-get update 2>&1 >> setup.log
-        #apt-get upgrade -y 2>&1 >> setup.log
+        apt-get update 2>&1 >> setup.log
+        apt-get upgrade -y 2>&1 >> setup.log
         if [ $(which mono) != "/usr/bin/mono" ]
           then
             echo Installing MONO runtime ...
             apt-get install mono-complete -y 2>&1 >> setup.log
           else
-            echo Mono $MONO_VER is installed
+            echo Mono $MONO_VER is installed ...
         fi
         if [ $(find /opt/ -name NzbDrone.exe) != "/opt/NzbDrone/NzbDrone.exe" ]
           then
             echo Installing NzbDrone package ...
             apt-get install nzbdrone -y 2>&1 >> setup.log
           else
-            echo Sonarr is already installed ... bye
-            exit 0
+            echo Sonarr is already installed ... nothing to do here ...
+            sleep 1
+            echo Bye!
+            return 0
         fi
         chown pi:pi -R /opt/NzbDrone
 ### This will verify if the file nzbdrone existsa in /etc/init.d/ folder and 
@@ -174,12 +176,12 @@ sonarr() {
             esac
             exit 0
 EOF
-          else  echo "the file exists: $SONARR_INIT_F"
+          else  echo The file $SONARR_INIT_F exists
         fi
         sudo chmod +x /etc/init.d/nzbdrone
         sudo update-rc.d nzbdrone defaults
         echo "Sonarr/NzbDrone is installed on your Raspberry Pi"
-        exit 0
+        return 0
 }
 couchpotato(){
         echo Installing CouchPotato ...
