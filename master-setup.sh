b44da74d99105cdf2b793e16a96e8ba4a82ff7c3
Victor Sky <skylandr@users.noreply.github.com>
2016-04-19 15:56:00 +0300

    Update master-setup.sh

--- master-setup.sh
+++ master-setup.sh
@@ -140,7 +140,8 @@ jackett_updt(){
 	wget -nc -O $JACKETT_TMP_F $JACKETT_DL
 	rm -f $JACKETT_TMP_HTM_F
 	systemctl stop jackett.service
-	rm -rf $JACKETT_D/*
+	sleep 2
+	rm -vrf $JACKETT_D/*
 	tar -xvf $JACKETT_TMP_F -C $JACKETT_D
 	systemctl start jackett.service
 	return 0
